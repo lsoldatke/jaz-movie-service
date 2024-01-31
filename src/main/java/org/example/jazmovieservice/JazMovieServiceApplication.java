08cf1cd8c8dbb906d874898c0099b82e6832a898
s24729-pj <s24729@pjwstk.edu.pl>
2024-01-31 17:21:55 +0100

    Initial commit

---
+++ src/main/java/org/example/jazmovieservice/JazMovieServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.jazmovieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazMovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazMovieServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/jazmovieservice/JazMovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jazmovieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazMovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
