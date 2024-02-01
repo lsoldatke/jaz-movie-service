ca3c8f0fd074f945e3133ef0093f8e6e55910a18
lukas <s24729@pjwstk.edu.pl>
2024-02-01 15:20:28 +0100

    Added REST controller

---
+++ src/main/java/org/example/jazmovieservice/controller/MovieServiceRestController.java
@@ -0,0 +1,59 @@
+package org.example.jazmovieservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.example.jazmovieservice.model.Movie;
+import org.example.jazmovieservice.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movieservice")
+public class MovieServiceRestController {
+    private final MovieService movieService;
+
+    public MovieServiceRestController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<Movie>> returnAllMovies() {
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+    @Operation(summary = "Get movie by ID", description = "Returns movie with given ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Movie not found")
+    })
+    @GetMapping("/movies/{id}")
+    public ResponseEntity<Optional<Movie>> returnMovie(@PathVariable Long id) {
+        Optional<Movie> movie = movieService.getMovieById(id);
+
+        if (movie.isPresent()) {
+            return ResponseEntity.ok(movie);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(movie);
+        }
+    }
+
+    @PostMapping("/movies")
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
+        if (movie.getId() == null) {
+            return ResponseEntity.ok(movie);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(movie);
+        }
+    }
+
+    @PutMapping("/movies/return/{id}")
+    public ResponseEntity<Optional<Movie>> changeAvailabilityToTrue(@PathVariable Long id) {
+        Optional<Movie> movie = movieService.setAvailability(id, true);
+        return ResponseEntity.ok(movie);
+    }
+}
