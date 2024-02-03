68e041306500894178b58a2439a71084ff0b0828
s24729-pj <s24729@pjwstk.edu.pl>
2024-02-03 11:47:45 +0100

    Changes in schema initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
-INSERT INTO movie(title, category, is_available) VALUES
-                                                     ('The Movie','COMEDY',true),
-                                                     ('The Movie 2','DOCUMENTARY',true),
-                                                     ('The Movie 3','THRILLER',false);
+INSERT INTO movie(title, category, is_available) VALUES ('The Movie','COMEDY',true);
+INSERT INTO movie(title, category, is_available) VALUES ('The Movie 2','DOCUMENTARY',true);
+INSERT INTO movie(title, category, is_available) VALUES ('The Movie 3','THRILLER',false);
